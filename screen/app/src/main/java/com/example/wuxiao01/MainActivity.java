ee496026a5529d31f9643ecc6bbd0e8973f430eb
武潇 <wuxiao0063@163.com>
2020-09-27 15:12:57 +0800

    补交

---
+++ screen/app/src/main/java/com/example/wuxiao01/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.wuxiao01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.wuxiao01.util.Utils;
+import android.content.Context;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView text_screen;
+    private TextView word;
+    private Button click;
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        text_screen=findViewById(R.id.name);
+         word=findViewById(R.id.word);
+
+
+
+
+
+
+
+        int dip_10= Utils.dip2x(this, 10L);
+        text_screen.setPadding(dip_10,dip_10,dip_10,dip_10);
+        text_screen.setBackgroundColor(0xff00ffff);
+        text_screen.setTextColor(0xff333333);
+       // showScreenInfo();  无法生成
+        word.setPadding(dip_10,dip_10,dip_10,dip_10);
+        word.setBackgroundColor(0xff0000);
+        word.setTextColor(0xff00ffff);
+        word.setTextSize(60);
+
+
+
+
+
+
+
+    }
+}
